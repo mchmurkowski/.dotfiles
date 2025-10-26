aab89fa17446eec01cbc8f374130515cc0e96d40
Michał Chmurkowski <4909359+mchmurkowski@users.noreply.github.com>
2025-10-26 13:32:05 +0100

    rewrite autocmds in fennel

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -1,48 +1,26 @@
 local function augroup(name)
-  return vim.api.nvim_create_augroup("mch_" .. name, { clear = true })
+  return vim.api.nvim_create_augroup(("mch_" .. name), {clear = true})
 end
-
-vim.api.nvim_create_autocmd({ "FocusGained", "TermClose", "TermLeave" }, {
-  group = augroup("reload_files"),
-  desc = "Reload files",
-  callback = function()
-    if vim.o.buftype ~= "nofile" then
-      vim.cmd("checktime")
-    end
-  end,
-})
-
-vim.api.nvim_create_autocmd("TextYankPost", {
-  group = augroup("highlight_yank"),
-  desc = "Highlight yanked text",
-  callback = function()
-    vim.highlight.on_yank()
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimResized", {
-  group = augroup("resize_splits"),
-  desc = "Resize splits on window resize",
-  callback = function()
-    local current_tab = vim.fn.tabpagenr()
-    vim.cmd("tabdo wincmd =")
-    vim.cmd("tabnext " .. current_tab)
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  group = augroup("wrap_text"),
-  desc = "Wrap text for text buffers",
-  pattern = { "gitcommit", "markdown" },
-  callback = function()
-    vim.opt_local.wrap = true
-  end,
-})
-
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = augroup("disable_highlight"),
-  desc = "Disable syntax highlighting with Tree Sitter",
-  callback = function()
-    vim.cmd("TSToggle highlight")
-  end,
-})
+local function _1_()
+  if (vim.o.buftype ~= "nofile") then
+    return vim.cmd("checktime")
+  else
+    return nil
+  end
+end
+vim.api.nvim_create_autocmd({"FocusGained", "TermClose", "TermLeave"}, {group = augroup("reload_files"), desc = "Reload files", callback = _1_})
+local function _3_()
+  return vim.highlight.on_yank()
+end
+vim.api.nvim_create_autocmd("TextYankPost", {group = augroup("highlight_yank"), desc = "Highlight yanked text", callback = _3_})
+local function _4_()
+  local current_tab = vim.fn.tabpagenr()
+  vim.cmd("tabdo wincmd =")
+  return vim.cmd(("tabnext" .. current_tab))
+end
+vim.api.nvim_create_autocmd("VimResized", {group = augroup("resize_splits"), desc = "Resize splits on window resize", callback = _4_})
+local function _5_()
+  vim.opt_local.wrap = true
+  return nil
+end
+return vim.api.nvim_create_autocmd("FileType", {group = augroup("wrap text"), desc = "Wrap text in text buffers", pattern = {"gitcommit", "markdown"}, callback = _5_})
